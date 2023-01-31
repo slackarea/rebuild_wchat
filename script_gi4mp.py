d0a4127607e1113f26beb761e2105d4ae371b0c9
Gi4mp <giampierogiacalone92@gmail.com>
2023-01-31 16:28:17 +0100

    Creato nuovo script che automatizza estrazione zip

---
+++ script_gi4mp.py
@@ -0,0 +1,144 @@
+import os
+import datetime
+from jinja2 import Template, Environment, FileSystemLoader
+from fpdf import FPDF
+import shutil
+import zipfile
+import hashlib
+
+#questa funzione servirà per estrarre il file zip contente la chat e ritorno l'hash del file zip
+def extract_chat():
+    md5_hash = hashlib.md5()
+    with open("prova.zip","rb") as f:
+    # Read and update hash in chunks of 4K
+        for byte_block in iter(lambda: f.read(4096),b""):
+            md5_hash.update(byte_block)
+    shutil.unpack_archive("prova.zip", "provazip")
+
+    return md5_hash.hexdigest()    
+
+
+
+
+def ios_chat(user,data):
+         
+    dataset = data [1:]
+    cleaned_data = []
+    lista = []
+
+    for line in dataset:
+        # Check, whether it is a new line or not
+        # If the following characters are in the line -> assumption it is NOT a new line
+        if '/' in line and ':' in line and ',' in line and '[' in line:
+            # grab the info and cut it out
+            date = line.split("[")[1].split(",")[0]
+            line2 = line[len(date):]
+            time = line2.split(",")[1].split("]")[0]
+            line3 = line2[len(time):]
+            sender = line3.split("]")[1].split(":")[0]
+            line4 = line3[len(sender):]
+            message = line4[5:-1]
+            message = message.replace(" \u200e", "" ).replace("\n", "")
+            position = 'received'
+            if sender[1:] != user:
+                position = 'sent'
+            cleaned_data.append([position, date, time[1:], sender[1:], message])
+            lista.append(date)
+        # else, assumption -> new line. Append new line to previous 'message'
+        else:
+            new = cleaned_data[-1][-1] + " " + line
+            cleaned_data[-1][-1] = new
+
+    return cleaned_data
+
+
+def android_chat(user,data):
+    print("android")
+
+
+
+
+def makeHTML(user,cleaned_data):
+    file_html_path = "index_wa.html"
+    if os.path.exists(file_html_path):
+        os.remove(file_html_path) 
+    i = open(file_html_path, mode='x', encoding="utf8")
+    file_loader = FileSystemLoader("templates")
+   
+    env = Environment(loader=file_loader)
+    start=env.get_template("start.txt")
+    end=env.get_template("end.txt")
+    day_template=env.get_template("day_template.txt")
+    message_template=env.get_template("message_template.txt")
+    media_template=env.get_template("media_template.txt")
+
+    i.write(start.render(name=user))
+
+    message_date = ""
+    p=""
+
+    for m in cleaned_data:
+        mess = m[4]
+
+        if (m[0]=="sent"):
+            p="end"
+        else:
+            p="start"
+
+        
+        if m[1] != message_date:
+            message_date = m[1]
+            i.write((day_template.render(day=m[1])))
+        
+        #posizione conterrà l'inizio della stringa "<allegato:"
+        position = m[4].find("<allegato:")
+        
+        if position>-1:
+            if(m[4].find(".jpg")>-1):
+                filename = m[4][position+11:len(m[4])-1]
+                mess =  "<a href=" + m[4][position+11:len(m[4])-1]+ " data-lightbox=" + m[4][position+11:len(m[4])-1]+ "  >" + "<img src=\""+m[4][position+11:len(m[4])-1]+"\">" +"</a>"
+    
+            elif(m[4].find(".opus") >-1 or m[4].find(".mp3") >-1):
+                mess = "<audio controls><source src="+ m[4][position+11:len(m[4])-1] +  " type='audio/ogg'>Your browser does not support the audio element.</audio>"
+            else:
+                mess = "<a href=\""+m[4][position+11:len(m[4])-1]+"\">"+m[4][position+11:len(m[4])-1]+"</a>"
+            i.write(media_template.render(position=p,type=m[0], message=mess,time=m[2][0:5]))
+            
+        else:
+            i.write(message_template.render(position=p,type=m[0], message=mess,time=m[2][0:5]))
+
+    i.write(end.render())
+    i.close()
+
+
+
+
+
+#main
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size = 15)
+
+hash=extract_chat()
+pdf.cell(200, 10, txt = "hash zip contente la chat estratta:"+str(hash),ln = 1, align = 'L')
+pdf.output("report.pdf", "F")
+cleaned_data=[]
+
+platform="ios"
+
+file_path = "chat.txt"
+with open(file_path, mode='r', encoding="utf8") as f:
+    data = f.readlines()
+    f.close()
+
+user =data[0].split(":")[2].split("]")[1][1:]
+    
+if(platform=="ios"):
+    cleaned_data=ios_chat(user, data)
+else:
+    cleaned_data=android_chat(user, data)
+
+makeHTML(user,cleaned_data)
+
+
